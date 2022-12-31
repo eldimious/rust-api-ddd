c58efd5f48cef534d523fda3562892f8eff9d4db
eldimious <botsaris.d@gmail.com>
2022-12-31 11:33:46 +0200

    add config mod

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use std::sync::Arc;
 use sqlx::migrate::Migrator;
-use crate::data::infrastructure::database::{get_db_pool, ping_db};
+use crate::configuration::configuration::Configuration;
+use crate::data::infrastructure::database::{check_db_connection, get_db_connection};
 use crate::data::repositories::user::user_data_store::UserDataStore;
 use crate::data::repositories::user::user_repository::UserRepository;
 use crate::domain::user::user_service::UserService;
@@ -11,19 +12,17 @@ mod presentation;
 mod domain;
 mod common;
 mod data;
+mod configuration;
 
 static MIGRATOR: Migrator = sqlx::migrate!();
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    dotenv::dotenv()
-        .ok()
-        .expect("Unable to find .env file. Create one based on the .env.example");
-    let port = env::var("PORT").expect("PORT must be set");
-    let db_pool = get_db_pool()
+    let config = Configuration::new();
+    let db_pool = get_db_connection()
         .await
         .expect("Unable to connect to the database");
-    ping_db()
+    check_db_connection()
         .await
         .expect("Unable to ping the database");
     MIGRATOR
@@ -35,7 +34,7 @@ async fn main() -> std::io::Result<()> {
     let user_repository = UserRepository::new(user_data_store);
     let user_service = Arc::new(UserService::new(user_repository));
 
-    let server = Server::new(port.parse().unwrap(), Services { user_service });
+    let server = Server::new(config.server.port.parse().unwrap(), Services { user_service });
     server.run().await;
 
     Ok(())
